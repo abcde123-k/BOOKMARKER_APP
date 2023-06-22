ef82a496265babab3cab9c895afe510dae0e4f54
Sagar-108 <99398965+Sagar-108@users.noreply.github.com>
2023-06-22 15:47:23 +0530

    Added Form validation
    Added Form validation  to login and signup pages and modified styling of login page

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,58 @@
 <template>
   <v-app>
     <toolBar v-if="!show" @showlogin="showloginpage = true, showsignuppage = false" @showsignup="showloginpage = false, showsignuppage = true"/>
+
+    <!-- error dialogs -->
+    <v-dialog v-model="wrongPass" width="auto">
+      <v-card>
+
+        <v-toolbar  class="pa-10 invalidCreds" >Wrong password entered</v-toolbar>
+
+      </v-card>
+    </v-dialog>
+
+        <v-dialog v-model="wrongEmail" width="auto">
+      <v-card>
+
+        <v-toolbar  class="pa-10 invalidCreds" >Invalid email entered</v-toolbar>
+
+      </v-card>
+    </v-dialog>
+
+        <v-dialog v-model="wrongUser" width="auto">
+      <v-card>
+
+        <v-toolbar  class="pa-10 invalidCreds" >User does not exist</v-toolbar>
+
+      </v-card>
+    </v-dialog>
+
     
     <!-- login page  -->
     <div v-if="showloginpage">
-      <form class="signup">
+            <form class="signup">
         <div class="middle">
-          <v-text-field label="Email" v-model="email"></v-text-field><v-spacer></v-spacer>
-          <v-text-field label="Password" v-model="password"></v-text-field><v-spacer></v-spacer>
-          <v-btn variant="outlined" class="center" append-icon="mdi-login" @click="login">Login</v-btn>
-          <!-- <v-btn class="ml-8 mt-n15" @click="showloginpage = false, showsignuppage = true">New User? -->
-            <v-btn variant="outlined" class="ml-13 mt-n15" @click="showloginpage = false, showsignuppage = true">
-              New User?
+          <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+          <v-text-field type="email" density="compact" placeholder="Email address" variant="outlined" prepend-inner-icon="mdi-email-outline" label="Email" v-model.lazy="email"   :rules="[v => !v || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'Enter a valid E-mail']" ></v-text-field><v-spacer></v-spacer>
+
+
+
+         
+          <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+        Password </div>
+          <v-text-field  density="compact"  variant="outlined" @click:append-inner="visible = !visible" prepend-inner-icon="mdi-lock-outline" :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'" label="Password" :type="visible ? 'text' : 'password'" v-model.lazy="password" placeholder="Enter your password" ></v-text-field><v-spacer></v-spacer>
+          
+   
+         
+          <v-btn block variant="outlined" variants="tonal"  color="blue-darken-3"  class="bg-blue-lighten-5 mb-6" append-icon="mdi-login" @click="login">Login</v-btn>
+
+            <v-btn block variant="outlined"  @click="showloginpage = false, showsignuppage = true">
+              New user? Sign Up Now!
               <v-tooltip
                 activator="parent"
                 location="bottom"
               >Signup Here</v-tooltip>
             </v-btn>
-          
         </div>
       </form>
     </div>
@@ -27,9 +62,9 @@
     <div v-if="showsignuppage">
         <form class="signup">
           <div class="middle">
-            <v-text-field label="Username" v-model="username"></v-text-field><v-spacer></v-spacer>
-            <v-text-field label="Enter Your Email" v-model="email"></v-text-field><v-spacer></v-spacer>
-            <v-text-field label="Password" v-model="password"></v-text-field><v-spacer></v-spacer>
+            <v-text-field label="Username" v-model="username" :rules="[v => !!v || 'Username is required']"></v-text-field><v-spacer></v-spacer>
+            <v-text-field label="Enter Your Email" v-model="email" :rules="[v => !v || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'Enter a valid E-mail']"></v-text-field><v-spacer></v-spacer>
+            <v-text-field label="Password" v-model="password" :rules="[v => !!v || 'password is required']"></v-text-field><v-spacer></v-spacer>
             <v-btn variant="outlined" class="ml-8 mt-4" append-icon="" @click="newaccount">Create Account</v-btn>
           </div>
         </form>
@@ -68,6 +103,9 @@ export default {
       const email = ref("");
       const password = ref("");
       const auth = setupAuth();
+      const wrongUser = ref(false);
+      const wrongPass = ref(false);
+      const wrongEmail = ref(false);
       provide('username', username);
       
 
@@ -76,13 +114,23 @@ export default {
 
         signInWithEmailAndPassword(auth, email.value, password.value)
           .then((cred) => {
-            console.log('user created:', cred.user);
+            console.log('user logged in:', cred.user);
             show.value=true;
             showloginpage.value = false;
             username.value = email.value.split('@')[0];
           })
           .catch((err) =>{
-            console.log(err.message)
+            console.log("error: ",err.message)
+
+            if(err.message.includes("user-not-found"))
+              wrongUser.value = true;
+
+            if(err.message.includes("wrong-password"))
+              wrongPass.value = true;
+
+            if(err.message.includes("wrong-email"))
+              wrongEmail.value = true;
+
           })
           
           onAuthStateChanged(auth, (user)=>{
@@ -142,8 +190,16 @@ export default {
       newaccount,
       showloginpage,
       showsignuppage,
+      wrongPass,
+      wrongUser,
+      wrongEmail,
     };
   },
+ data(){
+  return{
+    visible: false,
+  }
+}
 };
 </script>
 
@@ -157,5 +213,12 @@ export default {
 .center {
   margin: 25%;
   margin-top: 20px;
+}
+
+.invalidCreds {
+text-align: center;
+background-color: white;
+color: red;
+
 }
 </style>
